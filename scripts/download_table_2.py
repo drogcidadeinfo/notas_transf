11d81f99c727b46194a2d913a3cf6f04da28ac9b
Drogaria Cidade <informatica@drogcidade.com.br>
2026-02-16 15:07:08 -0300

    Create download_table_2.py

---
+++ scripts/download_table_2.py
@@ -0,0 +1,116 @@
+import os
+import time
+import logging
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+# set up logging config
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Environment variables
+username = os.getenv("username")
+password = os.getenv("password")
+
+if not username or not password:
+    raise ValueError("Environment variables 'user' and/or 'password' not set.")
+
+today = datetime.now()
+first_day_of_year = datetime(today.year, 1, 1)
+yesterday = today - timedelta(days=1)
+
+inicio = first_day_of_year.strftime("%d%m%Y")
+fim = yesterday.strftime("%d%m%Y")
+
+download_dir = os.getcwd()  
+
+# set up chrome options for headless mode/configure download behavior
+chrome_options = Options()
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--start-maximized")  # Maximize window
+
+prefs = {
+    "download.default_directory": download_dir,  # set download path
+    "download.prompt_for_download": False,  # disable prompt
+    "directory_upgrade": True,  # auto-overwrite existing files
+    "safebrowsing.enabled": False,  # disable safe browsing (meh)
+    "safebrowsing.disable_download_protection": True
+}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("--unsafely-treat-insecure-origin-as-secure=http://drogcidade.ddns.net:4647/sgfpod1/Login.pod")
+
+# initialize webdriver
+driver = webdriver.Chrome(options=chrome_options)
+
+# start download process 
+try:
+    logging.info("Navigate to the target URL and login")
+    driver.get("http://drogcidade.ddns.net:4647/sgfpod1/Login.pod")
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_cod_usuario"))).send_keys(username)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "nom_senha"))).send_keys(password)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login"))).click()
+
+    # wait til page loads completely
+    WebDriverWait(driver, 10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
+    time.sleep(10)
+
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.F11)
+    time.sleep(2)
+
+    # access "Compras Fornecedores"
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "sideMenuSearch")))
+    driver.find_element(By.ID, "sideMenuSearch").send_keys("Compras Fornecedores")
+    driver.find_element(By.ID, "sideMenuSearch").click()
+    driver.implicitly_wait(2)
+
+    driver.find_element(By.CSS_SELECTOR, '[title="Compras Fornecedores"]').click()
+
+    WebDriverWait(driver, 10).until(lambda x: x.execute_script("return document.readyState === 'complete'"))
+
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "agrup_fil_2"))).click()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "tabTabdhtmlgoodies_tabView1_2"))).click()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "selecao_periodo_1"))).click()
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "chk_cons_entrans"))).click()
+
+    # start and end dates
+    driver.find_element(By.ID, "dat_inicio").send_keys(inicio)
+    driver.find_element(By.ID, "dat_fim").send_keys(fim)
+
+    # report format; downloads xls file
+    driver.find_element(By.ID, "saida_4").click()  
+
+    # trigger report download
+    logging.info("Triggering report download...")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "runReport"))).click()
+
+    # log download start
+    logging.info("Download has started.")
+    # wait for download to complete
+    time.sleep(40)
+
+    # get the most recent downloaded file
+    files = os.listdir(download_dir)
+    downloaded_files = [f for f in files if f.endswith('.xls')]
+    if downloaded_files:
+        # sort files by modifi time
+        downloaded_files.sort(key=lambda x: os.path.getmtime(os.path.join(download_dir, x)))
+        most_recent_file = downloaded_files[-1]  # get the most recent file
+        downloaded_file_path = os.path.join(download_dir, most_recent_file)
+
+        # log the final file path and size
+        file_size = os.path.getsize(downloaded_file_path)
+        logging.info(f"Download completed successfully. File path: {downloaded_file_path}, Size: {file_size} bytes")
+    else:
+        logging.error("Download failed. No files found.")
+
+finally:
+    driver.quit()
