1a033e21a199a13f9b912a9d42b07b11e20223ec
Drogaria Cidade <informatica@drogcidade.com.br>
2026-07-20 11:27:58 -0300

    Increase sleep duration during download process

--- scripts/download_table.py
+++ scripts/download_table.py
@@ -99,7 +99,7 @@ try:
     # log download start
     logging.info("Download has started.")
     # wait for download to complete
-    time.sleep(20)
+    time.sleep(50)
 
     # get the most recent downloaded file
     files = os.listdir(download_dir)
